b2e5d2df408cf5265067909a255b03d752aa444f
carrotflakes <carrotflakes@gmail.com>
2015-09-17 21:53:29 +0900

    WIP

--- src/poler.lisp
+++ src/poler.lisp
@@ -11,9 +11,14 @@
 ;;; structures
 (defstruct operator
   symbol
+  fix
   associativity
+  arity
   precedence
   replaced-symbol)
+
+(defmethod print-object ((object operator) stream)
+  (format stream "#< ~a >" (operator-symbol object)))
 
 
 ;;; special variables
@@ -23,49 +28,90 @@
 
 
 ;;; auxiliary functions
+(defun part-close (part)
+  (cons 'closed part))
+
+(defun part-closed-p (part)
+  (eq (first part) 'closed))
+
 (defun fix (precedence part-list)
   (if (cdr part-list)
-      (let ((first (first part-list))
-            (second (second part-list)))
+      (let ((part1 (first part-list))
+            (part2 (second part-list)))
+        (when (part-closed-p part2) ; part2 is closed
+          (rplacd part-list (fix precedence (cdr part-list)))
+          (setf part2 (second part-list))
+          (when (part-closed-p part2)
+            (return-from fix part-list)))
         (cond
-          ((<= precedence (operator-precedence (first second)))
-           (rplacd (last second) (list first))
+          ((and (<= precedence (operator-precedence (first part2))) ; part2 is an operator can takes part1
+                (not (part-closed-p part2))
+                (part-closed-p part1))
+           (rplacd (last part2) (list (cdr part1)))
+           ;; close
+           (unless (and (eq (operator-fix (first part2)) :prefix)
+                        (< (- (length part2) 1)
+                           (or (operator-arity (first part2)) most-positive-fixnum)))
+             (setf (second part-list) (part-close (second part-list))))
            (fix precedence (cdr part-list)))
-          (t
+          (t ; part2 is an operator cannot takes part1
            part-list)))
       part-list))
 
 (defun lookup (symbol)
-  (find symbol *operators* :key #'operator-symbol :test #'eq))
+  (and (symbolp symbol)
+       (find symbol *operators* :key #'operator-symbol :test #'eq)))
+
+(defun part-close-prefix-* (part)
+  (if (and (operator-p (first part))
+           (eq (operator-fix (first part)) :prefix)
+           (null (operator-arity (first part))))
+      (part-close part)
+      part))
 
 ;; transform an infix-form into a tree
 (defun build-tree (tokens &optional part-list)
-  (if tokens
-      (let* ((token (pop tokens))
-             (operator (lookup token)))
-        (build-tree
-         tokens
-         (cond
-           ((null operator)
-            (list* (cond
-                     ((not (and *recursive*
-                                (consp token)))
-                      token)
-                     ((eq (car token) 'quote)
-                      (second token))
-                     (t
-                      (build-tree token)))
-                   part-list))
-           ((eq (operator-associativity operator) :pre)
-            (list* (list operator) part-list))
-           (t
-            (setf part-list (fix (operator-precedence operator) part-list))
-            (if (and (member (operator-associativity operator) '(:non :left :right))
-                     (consp (first part-list))
-                     (eq (first (first part-list)) operator))
-                part-list
-                (list* (list operator (first part-list)) (cdr part-list)))))))
-      (first (fix -1 part-list))))
+  (if
+   tokens
+   (let* ((token (car tokens))
+          (operator (lookup token)))
+     (build-tree
+      (cdr tokens)
+      (cond
+        ((null operator) ; token is not an operator
+         (cons (cond
+                 ((or (not *recursive*) (not (consp token))) ; operand
+                  (part-close token))
+                 ((eq (car token) 'quote) ; operand by quote
+                  (part-close (second token)))
+                 (t ; paren
+                  (build-tree token)))
+               part-list))
+        ((eq (operator-fix operator) :prefix)  ; prefix
+         (cons (list operator) part-list))
+        (t  ; infix or postfix
+         (setf part-list (fix (operator-precedence operator) part-list))
+         (let ((part1 (first part-list)))
+           (cond
+             ((and (eq (operator-fix operator) :infix) ; infix union
+                   (part-closed-p part1)
+                   (consp (cdr part1))
+                   (eq (second part1) operator)
+                   (not (eq (operator-arity operator) (- (length part1) 2))))
+              (setf (first part-list) (cdr part1)) ; unclose
+              part-list)
+             ((eq (operator-fix operator) :postfix) ; postfix
+              (cons (part-close (list operator (cdr part1))) (cdr part-list)))
+             ((eq (operator-fix operator) :infix) ; infix
+              (when (null part1)
+                (error "No operand at left of an infix operator."))
+              (cons (list operator (cdr part1)) (cdr part-list)))))))))
+   (let ((part-list (fix -1 part-list)))
+     (when (/= 1 (length part-list))
+       (error "Root is must only one."))
+     (unless (part-closed-p (first part-list))
+       (error "Invalcid infix form"))
+     (part-close-prefix-* (first part-list)))))
 
 ;; (left-associate '+ '(1 2 3)) => (+ (+ 1 2) 3)
 (defun left-associate (symbol arguments &optional associated)
@@ -94,42 +140,54 @@
         ((:right)
          (right-associate (operator-replaced-symbol (first tree))
                           (mapcar #'apply-operator (cdr tree))))
-        ((:non :pre :post)
+        (otherwise
          (list* (operator-replaced-symbol (first tree))
                 (mapcar #'apply-operator (cdr tree)))))
       tree))
+
+(defun new-operator (source)
+  (multiple-value-bind (fix associativity arity)
+      (case (second source)
+        (:infix-l    (values :infix :left nil))
+        (:infix-r    (values :infix :right nil))
+        (:infix-2    (values :infix nil 2))
+        (:infix-*    (values :infix nil nil))
+        (:prefix-1   (values :prefix nil 1))
+        (:prefix-*   (values :prefix nil nil))
+        (:postfix-1  (values :postfix nil 1))
+        (otherwise   (let ((symbol-name (symbol-name (second source))))
+                       (if (string= symbol-name "PREFIX-" :end1 7)
+                           (values :prefix nil (parse-integer symbol-name :start 7))
+                           (error "Bad operator type.")))))
+    (make-operator :symbol (first source)
+                   :fix fix
+                   :associativity associativity
+                   :arity arity
+                   :precedence (or (third source) most-positive-fixnum)
+                   :replaced-symbol (or (fourth source)
+                                        (first source)))))
 
 (defun parse-arguments (args)
   (let ((operators '()))
     (loop
        for operator-source in args
        while (listp operator-source)
-       do (push (make-operator
-                 :symbol (first operator-source)
-                 :associativity (or (second operator-source) :left)
-                 :precedence (or (third operator-source) most-positive-fixnum)
-                 :replaced-symbol (or (fourth operator-source)
-                                      (first operator-source)))
-                operators)
+       do (push (new-operator operator-source) operators)
        (pop args))
     (values operators
             (getf args :recursive t)
             (getf args :decorate nil))))
 
-;;; main macros
-(defmacro define-poler (name &body args)
-  (multiple-value-bind (operators recursive decorator) (parse-arguments args)
-    `(defmacro ,name (&body infix-form)
-       (let ((*operators* ',operators)
-             (*recursive* ',recursive))
-         ,(if decorator
-              `(list ,decorator (apply-operator (build-tree infix-form)))
-              `(apply-operator (build-tree infix-form)))))))
 
+;;; main macros
 (defmacro polish (infix-form &body args)
   (multiple-value-bind (operators recursive decorator) (parse-arguments args)
     `(let ((*operators* ',operators)
            (*recursive* ',recursive))
        ,(if decorator
-            `(list ,decorator (apply-operator (build-tree ,infix-form)))
-            `(apply-operator (build-tree ,infix-form))))))
+            `(list ,decorator (apply-operator (cdr (build-tree ,infix-form))))
+            `(apply-operator (cdr (build-tree ,infix-form)))))))
+
+(defmacro define-poler (name &body args)
+  `(defmacro ,name (&body infix-form)
+     (polish infix-form ,@args)))
